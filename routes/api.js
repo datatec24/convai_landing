bbc87b339e86d87ef63d27d2848b43e5eda19f98
datatec24 <maxime.le.dantec.24@gmail.com>
2016-10-20 17:34:16 +0200

    ok

--- routes/api.js
+++ routes/api.js
@@ -31,42 +31,42 @@ var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 //     res.json(post);
 //   });
 // });
-
-router.get('/', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
-
-
-router.post('/', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      email: req.body.email,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
-  });
-});
+// 
+// router.get('/', function(req, res) {
+//   fs.readFile(COMMENTS_FILE, function(err, data) {
+//     if (err) {
+//       console.error(err);
+//       process.exit(1);
+//     }
+//     res.json(JSON.parse(data));
+//   });
+// });
+//
+//
+// router.post('/', function(req, res) {
+//   fs.readFile(COMMENTS_FILE, function(err, data) {
+//     if (err) {
+//       console.error(err);
+//       process.exit(1);
+//     }
+//     var comments = JSON.parse(data);
+//     // NOTE: In a real implementation, we would likely rely on a database or
+//     // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+//     // treat Date.now() as unique-enough for our purposes.
+//     var newComment = {
+//       id: Date.now(),
+//       email: req.body.email,
+//     };
+//     comments.push(newComment);
+//     fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+//       if (err) {
+//         console.error(err);
+//         process.exit(1);
+//       }
+//       res.json(comments);
+//     });
+//   });
+// });
 
 
 
